aef8659404d61fa4fd2058ba9de31d2f6a2a88a5
Gustavo Ferreira <gustavofrsnt@gmail.com>
2022-09-11 23:38:56 -0300

    Create Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Main from "./components/Main"
 import Header from "./components/Header"
+import Input from "./components/Input"
 
 function App() {
   return (
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,7 @@
+import "./style.css"
+
+function Input(props) {
+  return <input type={props.type} placeholder={props.placeholder}/>
+}
+
+export default Input
